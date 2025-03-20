26baaea4afa40c17a544bf95b1d0a6eedaf9e584
Baldeabdoullah <abdoullahbalde@gmail.com>
2025-03-20 15:48:06 +0000

    initialisation

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="text-center font-bold text-3xl text-red-800 my-3">
+      REACT COOK
+    </div>
+  );
+};
+
+export default Title;
