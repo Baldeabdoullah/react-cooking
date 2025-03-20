f9bb869f31c8fa2f1070ddbe4ddb772761da4493
Baldeabdoullah <abdoullahbalde@gmail.com>
2025-03-20 21:38:05 +0000

    fin

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Title from "./components/Title";
-import Search from "./components/Search";
+
 import Card from "./components/Card";
 
 const App = () => {
   return (
     <div>
       <Title />
-      <Search />
+
       <Card />
     </div>
   );
